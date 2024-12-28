fd24b24d93549c1dff20b64bb349e7a0b640540f
Lucas Edson <lucasedson654@gmail.com>
2024-12-28 17:41:16 -0300

    Feat: Get Link Endpoint

---
+++ handler/getLink.go
@@ -0,0 +1,27 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lucasedson/go-shortener-link/config"
+	"github.com/lucasedson/go-shortener-link/schemas"
+)
+
+func GetLink(c *gin.Context) {
+
+	// shortLink := c.Param("shortLink")
+
+	var link schemas.Link
+
+	// config.GetDB().Where("short_link is ?", shortLink).First(&link)
+	config.GetDB().Where("short_link LIKE ?", "%"+c.Param("shortLink")).First(&link)
+	print(link.Url)
+
+	if link.Url == "" {
+		print(link.Url)
+		c.JSON(404, gin.H{"message": "link not found"})
+		return
+	}
+
+	c.JSON(200, gin.H{"url": link.Url})
+
+}
--- router/routes.go
+++ router/routes.go
@@ -9,6 +9,7 @@ func initializeRoutes(router *gin.Engine) {
 	prefix := "/api/v1"
 	router.GET(prefix+"/links", handler.GetLinks)
 	router.POST(prefix+"/link", handler.CreateLink)
+	router.GET(prefix+"/link/:shortLink", handler.GetLink)
 
 	router.GET("/:shortLink", handler.RedirectLink)
 }
